43f34df9d4f8a58b47330d0f6281affbc08696de
Shruti sahu <sahushruti2020@gmail.com>
2025-11-29 03:54:46 +0530

    fourth

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images:{
+    remotePatterns:[
+      {hostname:"lh3.googleusercontent.com"}
+    ]
+  }
   /* config options here */
 };
 
